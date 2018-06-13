6784409e9d8f500e7c0c04eb89a6c9908daa9057
VincenzoTranchita <v.tranchita@contrader.it>
2018-06-13 17:39:31 +0200

    IndexController

---
+++ src/main/controller/IndexController.java
@@ -0,0 +1,12 @@
+package main.controller;
+
+import main.MainDispatcher;
+
+public class IndexController implements Controller {
+
+
+    @Override
+    public void doControl(Request request) {
+        MainDispatcher.getInstance().callView("Index", request);
+    }
+}
---
+++ src/main/view/IndexView.java
@@ -0,0 +1,63 @@
+package main.view;
+
+import main.MainDispatcher;
+import main.controller.Request;
+
+import java.util.Scanner;
+
+public class IndexView implements View{
+
+    private int choice;
+    private String nomeUtente;
+    private String password;
+
+    @Override
+    public void showResults(Request request) {
+
+    }
+
+    @Override
+    public void showOptions () {
+
+        System.out.println("Benvenuto in pCarpet");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("-------MENU-------");
+        System.out.println("");
+        System.out.println("1) Esegui il login");
+        System.out.println("2) Iscriviti");
+        this.choice = Integer.parseInt(getInput());
+
+    }
+
+    @Override
+    public String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    @Override
+    public void submit() {
+
+        if(choice == 1)
+        {
+            System.out.println("-----LOGIN----");
+            System.out.println("Nome utente:");
+            nomeUtente = getInput();
+            System.out.println("Password:");
+            password = getInput();
+
+            Request request = new Request();
+            request.put("nomeUtente", nomeUtente);
+            request.put("password", password);
+
+            MainDispatcher.getInstance().callAction("Home", "doControl", request);
+        }
+        else if (choice == 2)
+        {
+            MainDispatcher.getInstance().callAction("Signup", "doControl", null);
+        }
+
+    }
+
+}
