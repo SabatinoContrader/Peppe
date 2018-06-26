cce97dcbab2c695203d7dd986cb1b41eeb6cb028
FedericoLettere <f.lettere@contrader.it>
2018-06-26 11:21:51 +0200

    remove library

--- src/main/java/com/pCarpet/model/Carplace.java
+++ src/main/java/com/pCarpet/model/Carplace.java
@@ -1,7 +1,5 @@
 package com.pCarpet.model;
 
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.Column;
 
@@ -9,13 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 import lombok.*;
 
--- src/main/java/com/pCarpet/model/Slot.java
+++ src/main/java/com/pCarpet/model/Slot.java
@@ -1,21 +1,13 @@
 package com.pCarpet.model;
 
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.Column;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 import lombok.*;
 
 @Data
