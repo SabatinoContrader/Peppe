05b3631f9ae3371b7a903169c295ef07d21610a9
Endri Kurushi <endri-kurushi@hotmail.com>
2018-10-01 14:03:36 +0200

    Fixed stop rows stuff

--- Frontend/src/app/theme/pages/aside-left-display-disabled/extension-stops/extension-stops.module.ts
+++ Frontend/src/app/theme/pages/aside-left-display-disabled/extension-stops/extension-stops.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { ExtensionStopsComponent } from './extension-stops.component';
 import { LayoutModule } from '../../../layouts/layout.module';
 import { AsideLeftDisplayDisabledComponent } from '../aside-left-display-disabled.component';
+import { ExtensionStopsRowComponent } from '../extension-stops-row/extension-stops-row.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
     {
@@ -19,13 +21,15 @@ const routes: Routes = [
 ];
 @NgModule({
     imports: [
-        CommonModule, RouterModule.forChild(routes), LayoutModule
+        CommonModule, RouterModule.forChild(routes), LayoutModule,
+        FormsModule
     ],
     exports: [
         RouterModule
     ],
     declarations: [
-        ExtensionStopsComponent
+        ExtensionStopsComponent,
+        ExtensionStopsRowComponent
     ],
     providers: [
 
