cc8c2184822d139e5b1ad6b9a2fb81f750b27076
VINCENZO TRANCHITA <v.tranchita@contrader.it>
2018-10-04 21:45:41 +0200

    collegamento al server

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,5 +2,7 @@
 //export const SERVER_API_URL = 'http://localhost:58708';
 //export const SERVER_API_URL = 'http://178.239.176.91';
 
-export const SERVER_API_URL = 'http://178.239.176.91:4200';
+export const SERVER_API_URL = 'https://178.239.176.44:58708';
 //export const SERVER_API_URL = 'http://178.239.176.91:4201';
+
+
